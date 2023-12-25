0fcc86627dfd83c1958dfb54a63a8a0d02af304a
Kai Chen(zhoucpu) <kchen513@outlook.com>
2023-12-25 17:14:23 +0800

    update scripts for applicablity test

--- causal4/downsample.py
+++ causal4/downsample.py
@@ -3,6 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from struct import pack, unpack, _clearcache
 from pathlib import Path
+import brainpy as bp
+import brainpy.math as bm
+bm.set_platform('cpu')
+from scipy.ndimage.filters import uniform_filter1d
+def get_sync_threshold(freq, N=100, binsize=2, T=1e3, dt=0.02):
+    async_pop = bp.neurons.PoissonGroup(N, freqs=freq)
+    runner = bp.DSRunner(async_pop, dt=dt, monitors=['spike'], progress_bar=False)
+    runner.run(T)
+    spike_data = np.asarray(np.nonzero(runner.mon['spike'])).astype(float)
+    spike_data[0] = runner.mon.ts[spike_data[0].astype(int)]
+    counts, bins = np.histogram(spike_data[0], bins=np.arange(0, T+binsize, binsize))
+    return uniform_filter1d(counts, size=3).max()
+
 # %%
 def downsample(fname:str, ofname:str, path:str, ra:float, force_regen=False):
     """downsample spike train data
@@ -50,6 +63,82 @@ def downsample(fname:str, ofname:str, path:str, ra:float, force_regen=False):
             counts, _ = np.histogram(spk_buff[:,0], bins=edges)
             if threshold is None:
                 threshold = counts.mean()+ra*counts.std()
+
+            # filter bins and save to file
+            sync_bin_idx = counts>threshold
+            slices = np.array(np.vsplit(spk_buff, np.cumsum(counts)), dtype=object)
+            new_spks = np.vstack(slices[:-1][~sync_bin_idx])
+            of.write(pack("d"*new_spks.shape[0]*new_spks.shape[1], *new_spks.flatten()))
+            _clearcache()
+
+            # post-process
+            slices = slices[-1]
+            del_spk_counts += counts[sync_bin_idx].sum()
+            total_spk_counts += counts.sum()
+
+            buff_char = f.read(n_spk_buff*8*2)
+            buff_len = int(len(buff_char)/8)
+
+        # process last slices
+        if slices.shape[0] <= threshold:
+            of.write(pack("d"*slices.shape[0]*slices.shape[1], *slices.flatten()))
+        else:
+            del_spk_counts += slices.shape[0]
+        total_spk_counts += slices.shape[0]
+    of.close()
+    print(f">> {total_spk_counts:d} spikes in total ...")
+    print(f">> {del_spk_counts:d} spikes to delete ...")
+    print(f">> {100*del_spk_counts/total_spk_counts:.2f}% spikes have been deleted!")
+#%%
+def downsample_v2(fname:str, ofname:str, path:str, N:int, force_regen=False):
+    """downsample spike train data
+
+    Args:
+        fname (str): filename of raw spike train data
+        ofname (str): filename of downsampled spike train data
+        path (str): path of spike train data
+        ra (float): downsample threshold
+    """
+    binsize = 10
+    n_spk_buff = 100_000
+    total_spk_counts = 0
+    del_spk_counts = 0
+    ifpath = Path(path+fname)
+    ofpath = Path(path+ofname)
+    if ofpath.exists() and not force_regen:
+        total_spk_counts = int(ifpath.stat().st_size/8/2)
+        del_spk_counts = int((ifpath.stat().st_size-ofpath.stat().st_size)/8/2)
+        print(f">> {ofpath} already exists ...")
+        print(f">> raw size :      {total_spk_counts:12.0f} spikes")
+        print(f">> filtered size : {del_spk_counts:12.0f} spikes")
+        print(f">> {100*del_spk_counts/total_spk_counts:.2f}% spikes have been deleted!")
+        return 0
+    of = open(ofpath, 'wb')
+    with open(ifpath, 'rb') as f:
+        buff_char = f.read(n_spk_buff*8*2)
+        buff_len = int(len(buff_char)/8)
+        threshold = None
+        slices = None
+        current_end_edge = 0.0
+
+        while buff_len>0:
+            # load data
+            spk_buff = np.array(unpack('d'*buff_len, buff_char)).reshape(-1,2)
+            if threshold is None:
+                firing_rate = spk_buff.shape[0]/spk_buff[-1,0]/N*1e3
+                threshold = np.mean([get_sync_threshold(firing_rate, N, binsize) for _ in range(10)])
+                print(f">> threshold = {threshold:.2f} ...")
+            tmax = spk_buff[-1,0]
+            edges = current_end_edge + \
+                np.arange(int((tmax-current_end_edge)/binsize)+1)*binsize
+            current_end_edge = edges[-1]
+            # histogram exclude (current_end_edge, tmax]
+
+            # calculate temporal histogram
+            if slices is not None and isinstance(slices, np.ndarray):
+                spk_buff = np.vstack([slices, spk_buff])
+            counts, _ = np.histogram(spk_buff[:,0], bins=edges)
+            counts = uniform_filter1d(counts, size=3)
 
             # filter bins and save to file
             sync_bin_idx = counts>threshold
